5210cf341af6594fdbf9703408a76ec37897bfff
Casey Peat <caseypeat@protonmail.com>
2022-11-14 21:07:48 +1300

    object detection demo done

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Detectron2-Lab
+# Detectron2 Lab
+
+# Object Detection
+
+## Inference using pretrained model
+
+### TODO:
+ - Copy weights file from xxx to ./object_detection/weights/model_final_b275ba.pkl
+ - Move to object detection directory `cd object_detection`
+ - Run objection detection inference script `python inference.py`
+
+## Labeling cards
+
+### TODO:
+ - Open `via_mod.html` using a web browser
+ - Add files from xxx 'cards_train'
+ - Add a 'card' region atribute of type drop down
+ - Add ids 1-3 with descriptions 'magic', 'yugio', and 'pokemon' (these are our classes)
+ - Use the rectangle region shape to annotate the card, and add a card classes using the drop down menu
+ - Repeat for all cards (or atleast ~30)
+ - Under the Annotation menu, select 'Export Annotations (COCO format)'
+ - Move the downloaded json file to 'object_detection/labels/cards_train.json'
+ - Repeat for 'cards_test' directory
+
+### What you should see:
+![via labeling demo](./images/via_labeling.png)
+
+## Training card object detector
+
+### TODO:
+ - Run object detection training script 'python train.py'
+ - 
+
+### What you should see:
+`[11/14 20:41:39 d2.utils.events]:  eta: 0:01:51  iter: 19  total_loss: 1.589  loss_cls: 1.439  loss_box_reg: 0.1429  loss_rpn_cls: 0.004999 loss_rpn_loc: 0.004084  time: 0.1136  data_time: 0.0048  lr: 4.9953e-06  max_mem: 2030M`
+
+`[11/14 20:41:42 d2.utils.events]:  eta: 0:01:47  iter: 39  total_loss: 1.481  loss_cls: 1.316  loss_box_reg: 0.1354  loss_rpn_cls: 0.003877  loss_rpn_loc: 0.003786  time: 0.1121  data_time: 0.0018  lr: 9.9902e-06  max_mem: 2030M`
+
+`[11/14 20:41:44 d2.utils.events]:  eta: 0:01:48  iter: 59  total_loss: 1.27  loss_cls: 1.114  loss_box_reg: 0.1318  loss_rpn_cls: 0.00459  loss_rpn_loc: 0.005093  time: 0.1140  data_time: 0.0019  lr: 1.4985e-05  max_mem: 2030M`
+
+`[11/14 20:41:46 d2.utils.events]:  eta: 0:01:47  iter: 79  total_loss: 0.974  loss_cls: 0.8542  loss_box_reg: 0.1418  loss_rpn_cls: 0.004968  loss_rpn_loc: 0.003661  time: 0.1148  data_time: 0.0019  lr: 1.998e-05  max_mem: 2030M`
+
+`[11/14 20:41:49 d2.utils.events]:  eta: 0:01:43  iter: 99  total_loss: 0.7066  loss_cls: 0.5575  loss_box_reg: 0.1449  loss_rpn_cls: 0.003264  loss_rpn_loc: 0.004681  time: 0.1136  data_time: 0.0018  lr: 2.4975e-05  max_mem: 2030M`
+
+
+## Inference using card model
+
+### TODO:
+ - Run object detection training script 'python inference_my_dataset.py'
+ - 
+
+### What you should see:
+![via labeling demo](./images/card_inference.png)
+
+# Instance Segmentation
---
+++ object_detection/inference_my_dataset.py
@@ -0,0 +1,85 @@
+import numpy as np
+import torch
+import cv2
+import matplotlib.pyplot as plt
+import json
+
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.engine.defaults import DefaultPredictor
+from detectron2.utils.visualizer import Visualizer
+
+
+def get_top_x_predictions(predictions, x):
+    predictions["instances"] = predictions["instances"][:x]
+
+def get_detections_above_confidence(predictions, conf=0.9):
+    scores = predictions["instances"].scores.cpu().numpy()
+    x = len(scores) - np.searchsorted(scores[::-1], conf)
+    get_top_x_predictions(predictions, x)
+
+def class_id2label(ids, classes):
+    labels = []
+    for id in ids:
+        labels.append(classes[id])
+    return labels
+
+def combine_label_and_score(labels, scores):
+    view_strings = []
+    for label, score in zip(labels, scores):
+        view_str = f"{label} - {score:.3f}"
+        view_strings.append(view_str)
+    return view_strings
+
+def get_categories(json_path):
+    with open(json_path, 'r') as file:
+        coco = json.load(file)
+    categories_list = ["" for i in range(len(coco["categories"]))]
+    for category in coco["categories"]:
+        categories_list[category["id"]-1] = category["name"]
+    return categories_list
+
+
+if __name__ == "__main__":
+    image = cv2.imread("../data/card.png")
+    image = cv2.imread("../data/cards/card_test/60734280312127420788862091329.png")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    cfg = get_cfg()
+    cfg.merge_from_file("./configs/train_rcnn_fpn.yaml")
+    cfg.MODEL.WEIGHTS = "./logs/cards/model_final.pth"
+
+    register_coco_instances(
+        name="my_dataset_train",
+        metadata={},
+        json_file="./labels/cards_train.json",
+        image_root="../data/cards/card_train")
+    register_coco_instances(
+        name="my_dataset_val",
+        metadata={},
+        json_file="./labels/cards_test.json",
+        image_root="../data/cards/card_test")
+
+    metadata = MetadataCatalog.get(cfg.DATASETS.TEST[0])
+    metadata.thing_classes = get_categories("./labels/cards_test.json")
+
+    predictor = DefaultPredictor(cfg)
+    predictions = predictor(image)
+    predictions["instances"] = predictions["instances"].to("cpu")
+
+    get_top_x_predictions(predictions, 1)
+    # get_detections_above_confidence(predictions, 0.8)
+
+    labels = class_id2label(predictions["instances"].pred_classes, metadata.thing_classes)
+    scores = predictions["instances"].scores.numpy()
+    view_strings = combine_label_and_score(labels, scores)
+
+    visualizer = Visualizer(image)
+    visualized_detection = visualizer.overlay_instances(
+        boxes=predictions["instances"].pred_boxes,
+        labels=view_strings,
+        ).get_image()
+
+    plt.imshow(visualized_detection)
+    plt.show()
